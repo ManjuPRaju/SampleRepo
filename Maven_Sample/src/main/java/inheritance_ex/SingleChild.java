879efda05f622b69d6151697976cb0a130dd72e9
hp <hp@192.168.1.36>
2024-10-18 20:39:05 +0530

    Done Single Inheritance pgm

---
+++ Maven_Sample/src/main/java/inheritance_ex/SingleChild.java
@@ -0,0 +1,17 @@
+package inheritance_ex;
+
+public class SingleChild extends SingleParent {
+
+	public static void main(String[] args) {
+	SingleChild obj=new SingleChild();
+	obj.display();
+	obj.display2();
+		// TODO Auto-generated method stub
+
+	}
+	public void display2()
+	{
+	System.out.println("Single Inheritance Child Class");
+	
+	}
+}
