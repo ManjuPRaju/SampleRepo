04909f3f13cffa570300452bb1693cf91d39966b
hp <hp@192.168.1.36>
2024-10-23 21:14:11 +0530

    AccessModifiers pgm done

---
+++ Maven_Sample/src/main/java/accessmodifier2/AccessModifierEx3.java
@@ -0,0 +1,13 @@
+package accessmodifier2;
+
+import accessmodifiers.AccessmodifierEx1;
+
+public class AccessModifierEx3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AccessmodifierEx1 obj4=new AccessmodifierEx1();
+		obj4.display1();
+	}
+
+}
---
+++ Maven_Sample/src/main/java/accessmodifiers/AccessmodifierEx2.java
@@ -0,0 +1,15 @@
+package accessmodifiers;
+
+public class AccessmodifierEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AccessmodifierEx1 obj2=new AccessmodifierEx1();
+		obj2.display1();
+		//obj2.display2();
+		obj2.display3();
+		obj2.display4();
+		
+	}
+
+}
